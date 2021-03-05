609f4c77b3163d72e841f850760a2049d103fd3d
Jonathan Ehwald <github@ehwald.info>
2021-03-05 06:07:41 +0100

    Add and use the actual design colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,43 @@ module.exports = {
       russo: "Russo One, sans-serif",
       inter: "Inter, sans-serif",
     },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: {
+        DEFAULT: "#FFFFFF"
+      },
+      black: {
+        DEFAULT: "#000000"
+      },
+      gray: {
+        "1": "#333333",
+        "2": "#4F4F4F",
+        "3": "#BDBDBD"
+      },
+      blue: {
+        DEFAULT: "#7288FF",
+        "1": "#DCEAFF"
+      },
+      purple: {
+        DEFAULT: "#BCC3FF",
+        "1": "#D8DCFF"
+      },
+      yellow: {
+        "1": "#FFF1D2",
+        "2": "#F2C94C"
+      },
+      red: {
+        "1": "#FFF4EF",
+        "2": "#FF9384"
+      },
+      orange: {
+        "1": "#FB9952"
+      },
+      green: {
+        "1": "#64E6D3"
+      }
+    }
   },
   variants: {},
   plugins: [],
