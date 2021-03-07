384120232cbac3d24718658683605afe88e6f0ef
Tim Weiland <tim@weiland-lahnstein.de>
2021-03-07 20:55:27 +0100

    Add FAQ section

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,39 +13,41 @@ module.exports = {
       transparent: "transparent",
       current: "currentColor",
       white: {
-        DEFAULT: "#FFFFFF"
+        DEFAULT: "#FFFFFF",
       },
       black: {
-        DEFAULT: "#000000"
+        DEFAULT: "#000000",
       },
       gray: {
-        "1": "#333333",
-        "2": "#4F4F4F",
-        "3": "#BDBDBD"
+        1: "#333333",
+        2: "#4F4F4F",
+        3: "#BDBDBD",
       },
       blue: {
         DEFAULT: "#7288FF",
-        "1": "#DCEAFF"
+        1: "#DCEAFF",
+        2: "#CCD6FF",
+        3: "#7288FF",
       },
       purple: {
         DEFAULT: "#BCC3FF",
-        "1": "#D8DCFF"
+        1: "#D8DCFF",
       },
       yellow: {
-        "1": "#FFF1D2",
-        "2": "#F2C94C"
+        1: "#FFF1D2",
+        2: "#F2C94C",
       },
       red: {
-        "1": "#FFF4EF",
-        "2": "#FF9384"
+        1: "#FFF4EF",
+        2: "#FF9384",
       },
       orange: {
-        "1": "#FB9952"
+        1: "#FB9952",
       },
       green: {
-        "1": "#64E6D3"
-      }
-    }
+        1: "#64E6D3",
+      },
+    },
   },
   variants: {},
   plugins: [],
